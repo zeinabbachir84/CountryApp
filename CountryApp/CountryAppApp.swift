485f7bf184f80819639bfda7fbd0c278ee424ee0
Zeinab Bachir <z>
2025-08-29 16:55:41 +0300

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Zeinab Bachir on 29/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CountryAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CountryAppTests/CountryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppTests.swift
+//  CountryAppTests
+//
+//  Created by Zeinab Bachir on 29/08/2025.
+//
+
+import Testing
+@testable import CountryApp
+
+struct CountryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
